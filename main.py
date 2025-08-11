db6361a64f4783be9ee0b7a385c7599f0b40011d
Bret Zanotelli <bret.zanotelli@yahoo.com>
2025-08-11 16:10:46 -0700

    create and apply circles. Circles all the fucking way down

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ while running:                          # main game loop
             running = False
 
     screen.fill((0, 0, 0)) # screen background color
+    player.draw(screen) # draw player
     pygame.display.flip()  #update display
 
     
@@ -21,7 +22,7 @@ def main():
           Screen width: {SCREEN_WIDTH}
           Screen height: {SCREEN_HEIGHT}""")
 
-
+player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
 
 if __name__ == "__main__":
     main()
